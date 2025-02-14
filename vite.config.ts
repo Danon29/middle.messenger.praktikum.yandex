de98891cf17aa3ff206796734dcacbb008496644
Usmanov Ruslan <usmanovruslan83@gmail.com>
2025-02-14 05:23:10 +0200

    Рефакторинг кода

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
-import fs from 'fs'
-import path from 'path'
 
 export default defineConfig({
   root: '.',
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve('C:/Praktikum/certs/key.pem')),
-      cert: fs.readFileSync(path.resolve('C:/Praktikum/certs/cert.pem'))
-    },
     port: 3000
   },
   build: {
